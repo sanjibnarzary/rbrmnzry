c3f62cc2b91b7f37c375a0fb1a6878dcaf98276c
Sanjib Narzary <o-._.-o@live.com>
2025-10-31 14:13:14 +0530

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,9 @@
 // This file is the root of your application, but it will be handled by middleware
 // to redirect to the appropriate language-specific page.
 export default function Home() {
+  if (typeof window !== "undefined") {
+    window.location.replace("/en");
+    return null;
+  }
   return null;
 }
