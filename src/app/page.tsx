32020308de40e266fe78f35f4e104d8a9691afff
Sanjib Narzary <o-._.-o@live.com>
2025-10-31 14:22:17 +0530

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,9 @@
 // This file is the root of your application, but it will be handled by middleware
 // to redirect to the appropriate language-specific page.
+import { redirect, RedirectType } from 'next/navigation'
+ 
+
 export default function Home() {
-  if (typeof window !== "undefined") {
-    window.location.replace("/en");
-    return null;
-  }
-  return null;
+  redirect('/en', RedirectType.replace);
+  //return null;
 }
